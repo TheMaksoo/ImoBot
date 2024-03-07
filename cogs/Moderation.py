38a4a4e980882324760dd9c9fef1f1f03804f2d9
TheMaksoo <39918032+TheMaksoo@users.noreply.github.com>
2024-03-08 00:41:35 +0100

    Update Moderation.py

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -6,19 +6,17 @@ import os
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
-target_user_id = 387317544228487168
-
-def is_target_user(ctx):
-    return ctx.author.id == target_user_id
+def is_target_role(ctx):
+    return any(role.name == "Manager" for role in ctx.author.roles)
 
 class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @bot.slash_command(guild_ids=[1102649117458563243])
-    @commands.check(is_target_user)
-    @commands.max_concurrency(1, per=commands.BucketType.user)
-    async def purge(self, ctx, limit: int, message_id: float):
+    @commands.check(is_target_role)
+    @commands.cooldown(1, 3600, commands.BucketType.user)
+    async def purge(self, ctx, limit: int):
         """
         Purge a specified number of messages from a channel.
         """
@@ -27,26 +25,12 @@ class Moderation(commands.Cog):
             await ctx.send("You don't have the permission to delete messages.")
             return
 
-        # Get the message that triggered the command
-        triggered_message = ctx.message
-
         # Get the messages from the channel
         messages = await ctx.channel.history(limit=limit + 1).flatten()
 
-        # Find the message with the specified ID
-        target_message = None
-        for message in messages:
-            if message.id == message_id:
-                target_message = message
-                break
-
-        # If the target message is not found, delete the specified number of messages
-        if target_message is None:
-            await ctx.send("Target message not found.")
-        # If the target message is found, delete the messages until the target message
-        else:
-            to_delete = [message for message in messages if message.id != target_message.id and message.id > triggered_message.id]
-            await ctx.channel.delete_messages(to_delete)
-            
+        # Delete the specified number of messages
+        to_delete = [message for message in messages if message.id > ctx.message.id]
+        await ctx.channel.delete_messages(to_delete)
+        
 def setup(bot):
    	bot.add_cog(Moderation(bot))
